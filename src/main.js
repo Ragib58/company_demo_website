c256aa547f71e9f79188be2401001ae2b8e6579c
Ragib58 <ragibshahriar58@gmail.com>
2023-06-11 15:27:39 +0600

    Third Commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,35 @@
-import { createApp } from 'vue'
+import {
+    initAccordions,
+    initCarousels,
+    initCollapses,
+    initDials,
+    initDismisses,
+    initDrawers,
+    initDropdowns,
+    initModals,
+    initPopovers,
+    initTabs,
+    initTooltips
+} from 'flowbite'
+import { createApp, onMounted } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initAccordions();
+    initCarousels();
+    initCollapses();
+    initDials();
+    initDismisses();
+    initDrawers();
+    initDropdowns();
+    initModals();
+    initPopovers();
+    initTabs();
+    initTooltips();
+})
 
 createApp(App)
  .use(router)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,33 +24,344 @@
     </div>
   </div>
 
-  <div class="mt-4 grid lg:grid-cols-2 w-[1200px]">
-    <div class="col-span-1">
-      <h3 class="font-bold">About Us</h3>
+  <div class="max-w-[1320px] md:py-[80] py-5 flex mx-auto md:flex-row flex-col">
+    <div class="mt-20 px-10 basis-[55%] pb-10">
+      <h1 class="text-2xl font-bold text-orange-400">
+        About<br />
+        <span class="font-semibold py-3 text-gray-900"
+          >WE TRANSFORM YOUR IDEA INTO SOLUTION</span
+        >
+      </h1>
+      <p class="py-3">
+        Since 2005, Our mission has been “Building IT solutions for Life”. We
+        believe in living in the Present and Future!<br />
+        Orangebd is an award-winning web development company in Bangladesh. We
+        pledge to offer a wide range of web applications and development
+        solutions to meet any kind of business demand. Organizations from many
+        Corporate sectors and Businesses have depended on us with full trust and
+        confidence.....
+      </p>
+      <a href="/about" class="text-orange-400">Read More..</a>
     </div>
-    <div class="col-span-1">
-      <h2 class="font-bold">WE TRANSFORM YOUR IDEA INTO SOLUTION</h2>
+
+    <div class="basis-[45%] w-full flex justify-center items-center group">
+      <img
+        class="absulate z-10 h-4/6 2xl:h-3/4 group-hover:animate-trans-right"
+        src="../assets/image1.png"
+      />
+      <img
+        class="rotate-12 h-4/6 2xl:h-5/6 relative -top-10 left-14 group-hover:animate-trans-left"
+        src="../assets/image2.png"
+      />
     </div>
-    <p class="">
-      Since 2005, Our mission has been “Building IT solutions for Life”. We
-      believe in living in the Present and Future!<br />
-      Orangebd is an award-winning web development company in Bangladesh. We
-      pledge to offer a wide range of web applications and development solutions
-      to meet any kind of business demand. Organizations from many Corporate
-      sectors and Businesses have depended on us with full trust and
-      confidence.....
-    </p>
-    <a href="/about" class="text-orange-400">Read More..</a>
   </div>
-  <div class="h-100 flex justify-center items-center group">
-    <img
-      class="absulate z-10 h-4/6 2xl:h-3/4 group-hover:animate-trans-right"
-      src="../assets/image1.png"
-    />
-    <img
-      class="rotate-12 h-4/6 2xl:h-5/6 relative -top-10 left-14 group-hover:animate-trans-left"
-      src="../assets/image2.png"
-    />
+
+  <div class="max-w-[1000px] md:py-[80] py-3 mx-auto">
+    <h1 class="text-2xl font-semibold text-orange-400 text-center">
+      Our Service
+    </h1>
+    <p class="text-center py-5">
+      Orangebd has 290+ clients of various categories including the Daily &
+      Online Newspaper, e-Paper, Digital Archiving, Government, Corporate,
+      College, School, Universities, Real Estate, IT Firm, NGO, Garments/ Buying
+      House and so on. Beside the national market we have clients across the
+      Philippines, United States, Canada and Russia as well.
+    </p>
+  </div>
+
+  <div
+    class="max-w-[1200px] mx-auto py-[50px] grid lg:grid-cols-3 sm:grid-cols-1 gap-6"
+  >
+    <div class="shadow-lg p-4 rounded-lg text-center">
+      <img
+        class="h-32 w-full object-contain animate-bounce"
+        src="../assets/image14.png"
+      />
+      <h3 class="text-xl font-medium py-2">E-Governance Solutions</h3>
+      <p class="text-sm text-gray-500">
+        Our focus is on improving public administration services and processes.
+      </p>
+    </div>
+    <div class="shadow-lg p-4 rounded-lg text-center">
+      <img
+        class="h-32 w-full object-contain animate-bounce"
+        src="../assets/image15.png"
+      />
+      <h3 class="text-xl font-medium py-2">Web Application Solutions</h3>
+      <p class="text-sm text-gray-500">
+        We at Orangebd, give top-quality service, work with a responsive team,
+        and we work to make clients' vision a reality.
+      </p>
+    </div>
+    <div class="shadow-lg p-4 rounded-lg text-center">
+      <img
+        class="h-32 w-full object-contain animate-bounce"
+        src="../assets/service2.png"
+      />
+      <h3 class="text-xl font-medium py-2">E-commerce Solutions</h3>
+      <p class="text-sm text-gray-500">
+        E-commerce website application is a solution that is geared to help a
+        company conduct business electonically.
+      </p>
+    </div>
+    <div class="shadow-lg p-4 rounded-lg text-center">
+      <img
+        class="h-32 w-full object-contain animate-bounce"
+        src="../assets/image17.png"
+      />
+      <h3 class="text-xl font-medium py-2">Software Development</h3>
+      <p class="text-sm text-gray-500">
+        Software development services are aimed at designing, engineering,
+        supporting, and evolving various software types.
+      </p>
+    </div>
+    <div class="shadow-lg p-4 rounded-lg text-center">
+      <img
+        class="h-32 w-full object-contain animate-bounce"
+        src="../assets/image18.png"
+      />
+      <h3 class="text-xl font-medium py-2">Mobile Application Solutions</h3>
+      <p class="text-sm text-gray-500">
+        Our mobile app developers have expertise in building solutions that meet
+        market demands and encourage business growth and expansion.
+      </p>
+    </div>
+    <div class="shadow-lg p-4 rounded-lg text-center">
+      <img
+        class="h-32 w-full object-contain animate-bounce"
+        src="../assets/service1.png"
+      />
+      <h3 class="text-xl font-medium py-2">News Portal Solutions</h3>
+      <p class="text-sm text-gray-500">
+        News portal is a platform where the mass people can access the latest
+        updates or national news.
+      </p>
+    </div>
+  </div>
+
+  <div class="max-w-[1000px] md:py-[80] py-3 mx-auto">
+    <h1 class="text-2xl font-semibold text-orange-400 text-center">
+      Our Project
+    </h1>
+
+    <div
+      id="default-carousel"
+      class="relative w-50% py-3"
+      data-carousel="slide"
+    >
+      <!-- Carousel wrapper -->
+      <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <!-- Item 1 -->
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img
+            src="../assets/image8.png"
+            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            alt="..."
+          />
+        </div>
+        <!-- Item 2 -->
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img
+            src="../assets/image9.png"
+            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            alt="..."
+          />
+        </div>
+        <!-- Item 3 -->
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img
+            src="../assets/image8.png"
+            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            alt="..."
+          />
+        </div>
+        <!-- Item 4 -->
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img
+            src="../assets/image9.png"
+            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            alt="..."
+          />
+        </div>
+        <!-- Item 5 -->
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img
+            src="../assets/image8.png"
+            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            alt="..."
+          />
+        </div>
+      </div>
+      <!-- Slider indicators -->
+      <div
+        class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2"
+      >
+        <button
+          type="button"
+          class="w-3 h-3 rounded-full"
+          aria-current="true"
+          aria-label="Slide 1"
+          data-carousel-slide-to="0"
+        ></button>
+        <button
+          type="button"
+          class="w-3 h-3 rounded-full"
+          aria-current="false"
+          aria-label="Slide 2"
+          data-carousel-slide-to="1"
+        ></button>
+        <button
+          type="button"
+          class="w-3 h-3 rounded-full"
+          aria-current="false"
+          aria-label="Slide 3"
+          data-carousel-slide-to="2"
+        ></button>
+        <button
+          type="button"
+          class="w-3 h-3 rounded-full"
+          aria-current="false"
+          aria-label="Slide 4"
+          data-carousel-slide-to="3"
+        ></button>
+        <button
+          type="button"
+          class="w-3 h-3 rounded-full"
+          aria-current="false"
+          aria-label="Slide 5"
+          data-carousel-slide-to="4"
+        ></button>
+      </div>
+      <!-- Slider controls -->
+      <button
+        type="button"
+        class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        data-carousel-prev
+      >
+        <span
+          class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 19l-7-7 7-7"
+            ></path>
+          </svg>
+          <span class="sr-only">Previous</span>
+        </span>
+      </button>
+      <button
+        type="button"
+        class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        data-carousel-next
+      >
+        <span
+          class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5l7 7-7 7"
+            ></path>
+          </svg>
+          <span class="sr-only">Next</span>
+        </span>
+      </button>
+    </div>
+  </div>
+
+  <div class="max-w-[1000px] md:py-[80] py-3 mx-auto">
+    <h1 class="text-4xl font-semibold text-orange-400 text-center">
+      ISO 9001:2015 Certified Company
+    </h1>
+    <p class="text-center py-5">
+      Our company has been ISO certified for Quality Management Systems since
+      2009. Orange Business Development Limited, Orangebd in short is a
+      developed organization and providing quality IT solutions to the different
+      Govt. and Private Organizations and solving their problems of aligning IT
+      with business direction.
+    </p>
+  </div>
+
+  <div class="flex items-center">
+    <svg
+      aria-hidden="true"
+      class="w-5 h-5 text-orange-400"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>First star</title>
+      <path
+        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+      ></path>
+    </svg>
+    <svg
+      aria-hidden="true"
+      class="w-5 h-5 text-orange-400"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Second star</title>
+      <path
+        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+      ></path>
+    </svg>
+    <svg
+      aria-hidden="true"
+      class="w-5 h-5 text-orange-400"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Third star</title>
+      <path
+        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+      ></path>
+    </svg>
+    <svg
+      aria-hidden="true"
+      class="w-5 h-5 text-orange-400"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Fourth star</title>
+      <path
+        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+      ></path>
+    </svg>
+    <svg
+      aria-hidden="true"
+      class="w-5 h-5 text-gray-300 dark:text-gray-500"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Fifth star</title>
+      <path
+        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+      ></path>
+    </svg>
   </div>
 </template>
 
